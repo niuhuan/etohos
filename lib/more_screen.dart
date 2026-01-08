a7554e778602b74ab5173a76ccffe2154358519d
niuhuan <20847533+niuhuan@users.noreply.github.com>
2026-01-08 16:46:12 +0800

    :sparkles: upgrade

--- lib/more_screen.dart
+++ lib/more_screen.dart
@@ -142,7 +142,7 @@ class MoreScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(12),
                   ),
                   child: Text(
-                    'Version 1.0.5',
+                    'Version 1.0.8',
                     style: TextStyle(
                       fontSize: 13,
                       color: colorScheme.onSurface.withOpacity(0.7),
